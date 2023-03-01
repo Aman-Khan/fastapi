b9dcfbec7b595fd2426db84d56526a73d3e3b408
d3xter <ak722442@gmail.com>
2023-03-01 19:22:02 +0530

    get all Posts(logged In User)

--- app/router/get.py
+++ app/router/get.py
@@ -1,9 +1,11 @@
+from typing import List
 from fastapi import APIRouter, HTTPException, status, Depends
 from ..database import get_db, Session
 from .. import models, oauth2, schemas
 
 router = APIRouter(tags=['GET'])
 
+#get post by post id(pid)
 @router.get('/{pid}', response_model=schemas.resUserPost)
 def getPost(pid: int, db: Session=Depends(get_db), user: oauth2.get_current_user = Depends()):
     user_id  = user.dict().get('user_id')
@@ -21,3 +23,13 @@ def getPost(pid: int, db: Session=Depends(get_db), user: oauth2.get_current_user
         else:
             return get_post
 
+#get all post
+@router.get('/user/posts', response_model=List[schemas.resUserPost])
+def getAllPost(db: Session = Depends(get_db), user: oauth2.get_current_user = Depends()):
+    user_id  = user.dict().get('user_id')
+    get_user = db.query(models.userCred).filter(models.userCred.email==user_id).first()
+    if get_user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid Credentails')
+    else:
+        posts = db.query(models.posts).filter(models.posts.id==get_user.id).all()
+        return posts
