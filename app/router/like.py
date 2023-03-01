0cd982bbd3d7340377e8c75223507ee89410fba7
d3xter <ak722442@gmail.com>
2023-03-01 22:46:09 +0530

    unlike feature is added

--- app/router/like.py
+++ app/router/like.py
@@ -22,7 +22,6 @@ def likePost(pid: int, db: Session = Depends(get_db), user: oauth2.get_current_u
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Post Not Found(Private Post)")
             
             elif check_for_like is not None:
-                print()
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Posted is already liked by you")
             
             likeModel = schemas.postLike(id=get_user.id, pid=get_post.pid)
@@ -32,3 +31,28 @@ def likePost(pid: int, db: Session = Depends(get_db), user: oauth2.get_current_u
             
             return {pid: "liked"}
             
+
+@router.delete('/post/unlike/{pid}')
+def unlikePost(pid: int, db: Session = Depends(get_db), user: oauth2.get_current_user = Depends()):
+    user_id  = user.dict().get('user_id')
+    get_user = db.query(models.userCred).filter(models.userCred.email==user_id).first()
+    if get_user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
+    else:
+        get_post = db.query(models.posts).filter(models.posts.pid==pid).first()
+        if get_post is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post Not Found")
+        else:  
+            check_for_like = db.query(models.votes).filter(models.votes.id==get_user.id, models.votes.pid==pid).first()
+            post_owner = db.query(models.userCred).filter(models.userCred.id==get_post.id).first()
+            
+            if post_owner.acs==1 and get_user.email != post_owner.email:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Post Not Found(Private Post)")
+
+            elif check_for_like is None:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Operation")
+
+            db.delete(check_for_like)
+            db.commit()
+            raise HTTPException(status_code=status.HTTP_202_ACCEPTED, detail="Post is unliked")
+
